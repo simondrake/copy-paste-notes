5b55f9c709ec638e27fc0f6f13c9f05f2c7f2a1a
Simon Drake <simondrake1990@gmail.com>
2023-08-10 12:43:01 +0100

    Support JSON format for list

--- cmd/list.go
+++ cmd/list.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/olekukonko/tablewriter"
+	"github.com/simondrake/copy-paste-notes/internal/notes"
 	"github.com/simondrake/copy-paste-notes/internal/sqlite"
 	"github.com/spf13/cobra"
 )
@@ -15,55 +17,74 @@ func newListCommand(client *sqlite.Client) *cobra.Command {
 		autoWrapText bool
 		raw          bool
 		titleOnly    bool
+		format       string
 	)
 
 	listCmd := &cobra.Command{
 		Use:   "list",
 		Short: "Lists all notes",
 		Run: func(_ *cobra.Command, _ []string) {
-			notes, err := client.ListNotes()
+			ns, err := client.ListNotes()
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "unable to list notes: ", err)
 				os.Exit(1)
 			}
 
-			table := tablewriter.NewWriter(os.Stdout)
-
-			if titleOnly {
-				table.SetHeader([]string{"ID", "Create Timestamp", "Title"})
-			} else {
-				table.SetHeader([]string{"ID", "Create Timestamp", "Title", "Description"})
-			}
-
-			table.SetAutoWrapText(autoWrapText)
-
-			for _, n := range notes {
-				if titleOnly {
-					table.Append([]string{fmt.Sprint(n.ID), n.CreateTimestamp, n.Title})
-					continue
+			switch format {
+			case "table":
+				outputTable(ns, titleOnly, autoWrapText, raw)
+			case "json":
+				e := json.NewEncoder(os.Stdout)
+				e.SetIndent("", "    ")
+				if err := e.Encode(ns); err != nil {
+					fmt.Fprintln(os.Stderr, "Unable to encode json response: ", err)
+					os.Exit(1)
 				}
-
-				if !raw {
-					spl := strings.Split(n.Description, "\\n")
-
-					out := make([]string, len(spl))
-					for i, s := range spl {
-						out[i] = strings.TrimSpace(s)
-					}
-
-					n.Description = strings.Join(out, "\n")
-				}
-
-				table.Append([]string{fmt.Sprint(n.ID), n.CreateTimestamp, n.Title, n.Description})
+			default:
+				fmt.Fprintln(os.Stderr, "unsupported format option")
+				os.Exit(1)
 			}
-
-			table.Render()
 		},
 	}
 
 	listCmd.Flags().BoolVarP(&autoWrapText, "autowrap", "w", false, "whether to auto wrap the text output")
 	listCmd.Flags().BoolVarP(&raw, "raw", "r", false, "Whether to show the raw text (e.g. don't parse the newline character as a literal newline)")
 	listCmd.Flags().BoolVar(&titleOnly, "title-only", false, "Whether to only show the title")
+	listCmd.Flags().StringVarP(&format, "format", "f", "table", "output format to use [table, json]")
 
 	return listCmd
+}
+
+func outputTable(ns []notes.Note, titleOnly bool, autoWrapText bool, raw bool) {
+	table := tablewriter.NewWriter(os.Stdout)
+
+	if titleOnly {
+		table.SetHeader([]string{"ID", "Create Timestamp", "Title"})
+	} else {
+		table.SetHeader([]string{"ID", "Create Timestamp", "Title", "Description"})
+	}
+
+	table.SetAutoWrapText(autoWrapText)
+
+	for _, n := range ns {
+		if titleOnly {
+			table.Append([]string{fmt.Sprint(n.ID), n.CreateTimestamp, n.Title})
+			continue
+		}
+
+		if !raw {
+			spl := strings.Split(n.Description, "\\n")
+
+			out := make([]string, len(spl))
+			for i, s := range spl {
+				out[i] = strings.TrimSpace(s)
+			}
+
+			n.Description = strings.Join(out, "\n")
+		}
+
+		table.Append([]string{fmt.Sprint(n.ID), n.CreateTimestamp, n.Title, n.Description})
+	}
+
+	table.Render()
 }
